685dd6fc4d740fa066d6e9efc8571f148c5b4713
HOANGDINHTUNG <mtung3365@gmail.com>
2025-01-05 18:45:27 +0700

    Add files via upload

--- ss14bt9.cpp
+++ ss14bt9.cpp
@@ -6,20 +6,30 @@ int main(){
 	fgets(chuoi,sizeof(chuoi),stdin);
 	chuoi[strcspn(chuoi,"\n")]='\0';
 	int length=(int)strlen(chuoi);
-	printf("Moi nhap vao ki tu can xoav : ");
+	printf("Moi nhap vao ki tu can xoa : ");
 	scanf("%c",&kytu);
-	for(int i=0;i<length;i++){
-		if(chuoi[i]==kytu){
-			for(int j=i;j<length;j++){
-				chuoi[j]=chuoi[j+1];
-			}
-			--length;
+	//2 vong for
+//	for(int i=0;i<length;i++){
+//		if(chuoi[i]==kytu){
+//			for(int j=i;j<length;j++){
+//				chuoi[j]=chuoi[j+1];
+//			}
+//			--i;
+//		}
+//	}
+	//1 vong for
+	int j=0;
+	for(int i=0;chuoi[i]!=0;i++){
+		if(chuoi[i]!=kytu){
+			chuoi[j++]=chuoi[i];
 		}
 	}
+	chuoi[j]='\0';
 	printf("Chuoi sau khi xoa ki tu %c la : ",kytu);
-	for(int i=0;i<length;i++){
+	for(int i=0;chuoi[i]!=0;i++){
 		printf("%c",chuoi[i]);
 	}
+	//printf("%s",chuoi);
 	return 0;
 }
 
